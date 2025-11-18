99b907d4a5fb467ba4c0af479ec84aba13984b92
MahmooudGaaber <mahmoud.gr231@gmail.com>
2025-11-18 14:06:29 +0200

    first commit

---
+++ src/main/java/com/embarkx/firstembarkspring/FirstEmbarkSpringApplication.java
@@ -0,0 +1,14 @@
+package com.embarkx.firstembarkspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstEmbarkSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstEmbarkSpringApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/embarkx/firstembarkspring/FirstEmbarkSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.embarkx.firstembarkspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstEmbarkSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
