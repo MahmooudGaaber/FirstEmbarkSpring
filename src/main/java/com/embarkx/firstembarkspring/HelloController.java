77af85dd698798032e8c6642ba184d5733ac11ac
MahmooudGaaber <mahmoud.gr231@gmail.com>
2025-11-18 17:00:01 +0200

    Sec 3 : First PostMapping Method Using PostMan

--- src/main/java/com/embarkx/firstembarkspring/HelloController.java
+++ src/main/java/com/embarkx/firstembarkspring/HelloController.java
@@ -1,6 +1,8 @@
 package com.embarkx.firstembarkspring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +15,9 @@ public class HelloController
         return "Hello World!";
     }
 
+    @PostMapping("/hello")
+    public String helloPost(@RequestBody String name){
+        return "Hello " + name + "!";
+    }
 
 }
